0e55a4d221c99f08fe01d9640a0afbf4aa3bf8af
mpetit480 <57752166+mpetit480@users.noreply.github.com>
2020-05-29 14:22:50 +0200

    Add files via upload

---
+++ netuse.bat
@@ -0,0 +1,7 @@
+::net use l: "\\livebox\samsung" /u:workgroup
+::net use l: "\\livebox" /u:workgroup
+::net use w: \\petma13-i194137\c$ /u:petma13-i194137\administrator interOP@194137
+net use o: \\192.168.1.14\e$ /u:192.168.1.14\mathieu s*marcande01
+::net use r: \\convert.ca.com\Thales
+
+::start r:
