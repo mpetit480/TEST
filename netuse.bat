37e52aedb227a729eb4754c7da1a37f4808ff4e2
mpetit480 <57752166+mpetit480@users.noreply.github.com>
2020-05-31 22:35:39 +0200

    Update netuse.bat

--- netuse.bat
+++ netuse.bat
@@ -3,5 +3,5 @@
 ::net use w: \\petma13-i194137\c$ /u:petma13-i194137\administrator interOP@194137
 net use o: \\192.168.1.14\e$ /u:192.168.1.14\mathieu s*marcande01
 ::net use r: \\convert.ca.com\Thales
-
+::
 ::start r:
