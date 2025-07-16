9eb637d6ca87a3e9b5b35114a499517abe5a83d4
mpetit480 <57752166+mpetit480@users.noreply.github.com>
2025-07-16 12:02:30 +0200

    Update Aloop.py

--- dags/Aloop.py
+++ dags/Aloop.py
@@ -1,16 +1,21 @@
 import airflow
-from airflow.models import DAG
+from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.utils.timezone import datetime
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
 
 jobyy=""
-with DAG(dag_id="Aloop", start_date=datetime(2021, 1, 1),schedule_interval="@once",catchup=False) as dag:
-  end = EmptyOperator(task_id="end")
-  start = EmptyOperator(task_id="start")
-  mylimit=11
-  for x in range(1,mylimit):
+with DAG(
+  dag_id="Aloop",
+  start_date=datetime(2021, 1, 1),
+  schedule_interval="@once",
+  catchup=False
+) as dag:
+   end = EmptyOperator(task_id="end")
+   start = EmptyOperator(task_id="start")
+   mylimit=11
+   for x in range(1,mylimit):
     for y in range(0,mylimit):
        a=str(x)+str(y)
        a=f"{x:0>4}"
